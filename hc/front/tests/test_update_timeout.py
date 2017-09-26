ed916ff0e826ac01e76c2c39c52931e901f3092b
ndero <john.waithaka@andela.com>
2017-09-26 10:29:11 +0300

    bg: fix update timeout failing test

--- hc/front/tests/test_update_timeout.py
+++ hc/front/tests/test_update_timeout.py
@@ -11,7 +11,7 @@ class UpdateTimeoutTestCase(BaseTestCase):
 
     def test_it_works(self):
         url = "/checks/%s/timeout/" % self.check.code
-        payload = {"timeout": 3600, "grace": 60, "reverse": 1800}
+        payload = {"timeout": 3600, "grace": 60, "reverse": 1800, "nag_interval": 60}
 
         self.client.login(username="alice@example.org", password="password")
         r = self.client.post(url, data=payload)
@@ -21,10 +21,11 @@ class UpdateTimeoutTestCase(BaseTestCase):
         print(check.timeout.total_seconds())
         assert check.timeout.total_seconds() == 3600
         assert check.grace.total_seconds() == 60
+        assert check.nag_interval.total_seconds() == 60
 
     def test_team_access_works(self):
         url = "/checks/%s/timeout/" % self.check.code
-        payload = {"timeout": 7200, "grace": 60, "reverse": 1800}
+        payload = {"timeout": 7200, "grace": 60, "reverse": 1800, "nag_interval": 60}
 
         # Logging in as bob, not alice. Bob has team access so this
         # should work.
